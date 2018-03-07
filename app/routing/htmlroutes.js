e5a63377b2242f90287aea6dcb3a1f279b7098e9
Adam Specker <specker@gmail.com>
2018-03-07 13:03:08 -0500

    integrating html pages into scripts

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -2,15 +2,15 @@ var path = require ('path');
 
 module.exports = function (app){
     app.get('/game', function (req,res) {
-        res.sendFile(path.join(__dirname,'/../public/gamepage.html'));
+        res.sendFile(path.join(__dirname,'/../public/game.html'));
     });
     app.get('*', function (req,res){
         res.sendFile(path.join(__dirname,'/../public/index.html'));
     });
     app.get('/results',(req,res)=>{
-        res.sendFile(path.join(__dirname,'/../public/score.html'))
+        res.sendFile(path.join(__dirname,'/../public/results.html'))
     });
-    app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'/../../testing.html'))
-    });
+    // app.get('*',(req,res)=>{
+    //     res.sendFile(path.join(__dirname,'/../../testing.html'))
+    // });
 }
