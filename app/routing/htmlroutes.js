e2db80d6ffbcbb4ecaab41eb45482f8262c5149e
Adam Specker <specker@gmail.com>
2018-03-07 14:23:48 -0500

    integrating scripts into page

--- app/routing/htmlroutes.js
+++ app/routing/htmlroutes.js
@@ -2,13 +2,13 @@ var path = require ('path');
 
 module.exports = function (app){
     app.get('/game', function (req,res) {
-        res.sendFile(path.join(__dirname,'/../public/game.html'));
+        res.sendFile(path.join(__dirname,'../game.html'));
     });
-    app.get('*', function (req,res){
-        res.sendFile(path.join(__dirname,'/../public/index.html'));
+    app.get('/', function (req,res){
+        res.sendFile(path.join(__dirname,'../index.html'));
     });
-    app.get('/results',(req,res)=>{
-        res.sendFile(path.join(__dirname,'/../public/results.html'))
+    app.get('/results',function (req,res){
+        res.sendFile(path.join(__dirname,'../results.html'))
     });
     // app.get('*',(req,res)=>{
     //     res.sendFile(path.join(__dirname,'/../../testing.html'))
--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const bodyParser = require ('body-parser');
 const express = require ('express');
+const fs = require ('fs');
+const path = require ('path');
 
 var app = express ();
 var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, 'app/public')));
+
 require ('./app/routing/apiroutes.js')(app);
 require ('./app/routing/htmlroutes.js')(app);
+
 
 app.listen(PORT, function () {
     console.log('Listening on '+PORT);
